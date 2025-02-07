2f96120e604c8aba28cf930dd3cacd7b269dbb2b
MATTYGILO <34808802+MATTYGILO@users.noreply.github.com>
2025-02-07 13:19:53 +0000

    Started demo 1 camera and LED

--- main.py
+++ main.py
@@ -8,41 +8,40 @@ path = "tmp/image.jpeg"
 
 def main():
     ard = Arduino("/dev/tty.usbmodem0000011")
-    # cam = Camera()
-    #
-    # for _ in range(100):
-    #
-    #     # Take a photo
-    #     cam.take_photo(path)
-    #
-    #     # Predict the image
-    #     prediction_image = predict_image(path)
-    #
-    #     # Get the predictions
-    #     prediction = prediction_image["predictions"]
-    #     result = prediction["result"]
-    #
-    #     scores = {}
-    #
-    #     for r in result:
-    #         label = r["label"]
-    #         score = r["score"]
-    #
-    #         # Check if the label is already in the scores
-    #         if label not in scores or scores[label] is None or scores[label] < score:
-    #             scores[label] = score
-    #
-    #
-    #     if "keyboard" in scores and scores["keyboard"] > 0.5:
-    #         print("Keyboard Detected!")
-    #         # Set the led to on
-    #         ard.send_cmd("led", 1)
-    #     else:
-    #         print("Keyboard Not Detected!")
-    #         # Set the led to off
-    #         ard.send_cmd("led", 0)
+    cam = Camera()
 
-    ard.send_cmd("bob", 9)
+    for _ in range(100):
+
+        # Take a photo
+        cam.take_photo(path)
+
+        # Predict the image
+        prediction_image = predict_image(path)
+
+        # Get the predictions
+        prediction = prediction_image["predictions"]
+        result = prediction["result"]
+
+        scores = {}
+
+        for r in result:
+            label = r["label"]
+            score = r["score"]
+
+            # Check if the label is already in the scores
+            if label not in scores or scores[label] is None or scores[label] < score:
+                scores[label] = score
+
+
+        if "keyboard" in scores and scores["keyboard"] > 0.5:
+            print("Keyboard Detected!")
+            # Set the led to on
+            ard.send_cmd("led", 1)
+        else:
+            print("Keyboard Not Detected!")
+            # Set the led to off
+            ard.send_cmd("led", 0)
+
 
 if __name__ == "__main__":
     main()
